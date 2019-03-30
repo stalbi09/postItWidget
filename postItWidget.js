c5caf3b59be5f25c784d3b4403d140ac939e966d
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-03-30 12:42:20 +0100

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -33,20 +33,7 @@ class PostItModel extends WidgetModel {
 		
 	}
 
-   /* c ici quon a la couleur par defaut du widget
-	this.mvc.mainstore  c ici quon enregitrele texte https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/select
-	mvc.main.app
-	text area c un element hmtl
-	creatPostIt(c,p) {
-		let postIt= new PostItWidget;
-		// est ce que c moi qui va definir aleatoirement le id du nouveau widget ou pas ?
-		// comment changer le vew de mon nouveau widget a partir dici?
-		// comment enregister le texte saisie par lutilisateur ensuite
-		//fontFamily pour la police c en caracte "" https://www.zonecss.fr/javascript-css/fontfamily-js.html
-		
-	}
-	*/
-
+   
 }
 
 class PostItView extends WidgetView {
@@ -65,11 +52,11 @@ class PostItView extends WidgetView {
 		this.try.header.innerHTML = "Post-it";
 		this.text=HH.create("textarea"); 
 		// on utilise value pour ecrire decu ou bien inner.HTML
-		SS.style(this.try.text, {"position": "relative", "width": "100px", "height": h + "100px", "backgroundColor": "yellow","fontFamily" : "Calibri", "overflow": "hidden" });
+		SS.style(this.try.text, {"position": "relative", "width": "100px", "height": h + "100px", "backgroundColor": "rgba(255, 246, 143, 1)"
+, "overflow": "hidden"});
 		//this.try.text.innerHTML="Veuillez saisir votre texte !";
 		this.try.stage.appendChild(this.try.text);
-// http://www.flatuicolorpicker.com/yellow-rgba-color-code pour les couleur en rgba
-// les noms des couleurs en js http://www.wwg1.com/pele-mele/couleurs/noms-couleurs-html.html
+
 
 
 		this.couleur= HH.create("input");
@@ -87,8 +74,7 @@ class PostItView extends WidgetView {
 		SS.style(this.try.ajout, {"userSelect": "none", "cursor": "pointer"});
 		Events.on(this.try.ajout, "click", event => this.try.mvc.controller.ajoutPostIt());
 		this.try.stage.appendChild(this.try.ajout);
-		 // est ce que c ici que lutilisateur saisie son texte?
-		// c ici quon fait ss background et tt
+		
 	}
 	
 	update(title, link) {
@@ -114,10 +100,7 @@ class PostItController extends WidgetController {
 		var p = this.try.mvc.view.police.value ;
 		SS.style(this.try.mvc.view.text, {"backgroundColor": c +"", "fontFamily" : p + ""});
 
-		//console.log("creer le nouveau post it");
-		//let postIt= new postItWidget;
-		//this.mvc.model.creatPostIt(c,p);
-		//c ici quon change la couleur et la police du widget
+		
 	}
 	
 	async load() {
