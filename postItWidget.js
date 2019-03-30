f630ae1109ceb589d3aedd9b56b881b916c5cf8a
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-03-30 12:24:53 +0100

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -37,7 +37,6 @@ class PostItModel extends WidgetModel {
 	this.mvc.mainstore  c ici quon enregitrele texte https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/select
 	mvc.main.app
 	text area c un element hmtl
-*/
 	creatPostIt(c,p) {
 		let postIt= new PostItWidget;
 		// est ce que c moi qui va definir aleatoirement le id du nouveau widget ou pas ?
@@ -46,7 +45,7 @@ class PostItModel extends WidgetModel {
 		//fontFamily pour la police c en caracte "" https://www.zonecss.fr/javascript-css/fontfamily-js.html
 		
 	}
-	
+	*/
 
 }
 
@@ -65,18 +64,28 @@ class PostItView extends WidgetView {
 		super.draw();
 		this.try.header.innerHTML = "Post-it";
 		this.text=HH.create("textarea"); 
-		this.try.text.innerHTML="Veuillez saisir votre texte !";
 		// on utilise value pour ecrire decu ou bien inner.HTML
-		SS.style(this.try.text, {"position": "relative", "width": "50px", "height": "50px", "overflow": "hidden", "backgroundColor": "rgba(255, 246, 143, 1)"});
+		SS.style(this.try.text, {"position": "relative", "width": "100px", "height": h + "100px", "backgroundColor": "yellow","fontFamily" : "Calibri", "overflow": "hidden" : });
+		//this.try.text.innerHTML="Veuillez saisir votre texte !";
 		this.try.stage.appendChild(this.try.text);
-		
-		var c = prompt("Veuillez choisir une couleur de fond");
-	 	var p = prompt("Veuillez choisir une police");
-		
-		this.ajout=HH.create("button");
-	        this.try.ajout.innerHTML = "Ajouter un Post-It";
+// http://www.flatuicolorpicker.com/yellow-rgba-color-code pour les couleur en rgba
+// les noms des couleurs en js http://www.wwg1.com/pele-mele/couleurs/noms-couleurs-html.html
+
+
+		this.couleur= HH.create("input");
+		SS.style(this.try.couleur, {"width" : "50%", "height" : "50%"});
+		this.couleur.setAttribute("type","text");
+
+		this.police= HH.create("input");
+		SS.style(this.try.police, {"width" : "50%", "height" : "50%"});
+		this.police.setAttribute("type","text");
+
+	 	
+
+	     this.ajout=HH.create("button");
+	     this.try.ajout.innerHTML = "changer la couleur et la police";
 		SS.style(this.try.ajout, {"userSelect": "none", "cursor": "pointer"});
-		Events.on(this.try.ajout, "click", event => this.try.mvc.controller.ajoutPostIt(c,p));
+		Events.on(this.try.ajout, "click", event => this.try.mvc.controller.ajoutPostIt());
 		this.try.stage.appendChild(this.try.ajout);
 		 // est ce que c ici que lutilisateur saisie son texte?
 		// c ici quon fait ss background et tt
@@ -101,7 +110,13 @@ class PostItController extends WidgetController {
 	}
 
 	ajoutPostIt() {
-		creatPostIt();
+		var c = this.try.mvc.view.couleur.value ;
+		var p = this.try.mvc.view.police.value ;
+		SS.style(this.try.mvc.view.text, {"backgroundColor": c +"", "fontFamily" : p + ""});
+
+		//console.log("creer le nouveau post it");
+		//let postIt= new postItWidget;
+		//this.mvc.model.creatPostIt(c,p);
 		//c ici quon change la couleur et la police du widget
 	}
 	
