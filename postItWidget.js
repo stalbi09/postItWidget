932abbf58649bb7ae76ed6b3e6a4134a73a74c4c
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-04-03 21:39:24 +0200

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -17,7 +17,7 @@ class PostItWidget extends Widget {
 	
 	async ready() {
 		super.ready();
-		restorePostIt();
+		this.try.mvc.controller.restorePostIt();
 		// c ici que jappelle la fonction restore
 		this.controller.load();
 	}
