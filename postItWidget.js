f6c57feb07a654f6f4a8db957062af5c5ca532de
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-04-05 22:05:06 +0200

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -254,8 +254,7 @@ class PostItController extends WidgetController {
 	}
 
 	changeColorPolice() {
-		console.log(this.try.mvc.view.couleurSelect.text);
-		console.log(this.try.mvc.view.policeSelect.text);
+		
 		var c = this.try.mvc.view.couleurSelect.value ;
 		var p = this.try.mvc.view.policeSelect.value ;
 		SS.style(this.try.mvc.view.text, {"fontFamily" : p + "","backgroundColor": c +""});
