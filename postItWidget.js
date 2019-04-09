5e9c9adf336021edd5a46ada485696ee505ed060
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-04-09 23:55:52 +0200

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -33,7 +33,7 @@ class PostItModel extends WidgetModel {
 		
 	}
 	
-	setUp() {
+	/*setUp() {
 		super.setUp();
 		
 		// ca prend la khaki par defaut sinon colordata 
@@ -52,7 +52,7 @@ class PostItModel extends WidgetModel {
 		
 		
 		
-	}
+	}*/
 	
        storeData(){
 		
@@ -89,12 +89,12 @@ class PostItModel extends WidgetModel {
 		
 	
 	this.textPostIt="";
-		
-	var cd=this.try.restoreCouleur() + "";
-	this.couleurPostIt ="khaki";
-	console.log(cd);
-	this.policePostIt="Calibri";
-	
+	setUp() {	
+		var cd=this.try.restoreCouleur() + "";
+		this.couleurPostIt ="khaki";
+		console.log(cd);
+		this.policePostIt="Calibri";
+	}
 		
 
    
