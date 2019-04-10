0c7bbf9d43043f18ea349607482e812ca82153dc
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-04-10 15:13:08 +0200

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -287,7 +287,7 @@ class PostItController extends WidgetController {
 		
 		this.try.changeColorPolice();
 		
-		document.body.removeChild(this.try.newpostit.view.stage);
+		document.body.removeChild(this.newpostit.view.stage);
 	}
 	
 	addListe(){
