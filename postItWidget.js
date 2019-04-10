23f7be7642ad9a5ad6357292d10280ebd7efd1ee
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-04-10 16:39:21 +0200

    Update postItWidget.js

--- postItWidget.js
+++ postItWidget.js
@@ -266,7 +266,7 @@ class PostItController extends WidgetController {
 		let p=new PostItWidget(i,this);
 		document.body.appendChild(p.view.stage);
 		i++;*/
-		this.loadWidget(newpostIt);
+		window.main.loadWidget(newpostIt);
 	}
 	
 	deletePostIt(){
