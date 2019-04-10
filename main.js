c1f4ad7360bc4bde9ea4b73e2d46d7cac88bef9c
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-04-10 16:09:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-window.addEventListener("load", event => new Main());
+window.addEventListener("load", event => {
+	window.Main = new Main();
+	window.Main.load();
+});
 
 class Main {
 	
 	constructor() {
+		trace("load dash");
 		this._name = "dash";
-		let widget = new PostItWidget(1, this);
-		document.body.appendChild(widget.view.stage);
+		this._widgets = new Map();
+		this._id = 0;
+	}
+	
+	load() {
+		this.loadWidget(postItWidget);
+		//this.loadWidget(SomeWidgetClass);
+	}
+	
+	loadWidget(classRef) {
+		let widget = new classRef(++this._id, this);
+		document.body.appendChild(widget.mvc.view.stage);
+		trace("new widget", widget.name);
+		this._widgets.set(widget.name, widget);
 	}
 	
 	async get(url) {
@@ -16,4 +32,14 @@ class Main {
 		return await Comm.urlrequest("https://node.nicopr.fr/" + this._name + url, {"method": "POST", "data": data});
 	}
 	
+	get appname() {
+		return this._name;
+	}
+	
+	get widgets() {
+		return this._widgets;
+	}
 }
+
+
+
