59799ee0f79fba0b88c2cb74bc9d1ac679096b7a
stalbi09 <47397928+stalbi09@users.noreply.github.com>
2019-05-07 17:41:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ class Main {
 		
 	load() {
 		var i;
-		var n=this._widgets.get("postit").mvc.model.restoreNombrePostIt();
+		var n=1; //this._widgets.get("postit").mvc.model.restoreNombrePostIt();
 		for(i=0;i<n;i++){
 			this.loadWidget(PostItWidget);
 			
